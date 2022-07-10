ee65527afb03a58c372cfdc9bd5baf6398aded11
filipmackiewicz <mackiewicz.filip@gmail.com>
2022-07-10 12:21:06 +0200

    enume xample

--- app.ts
+++ app.ts
@@ -1,8 +1,20 @@
+
+enum Role {
+    ADMIN,
+    READ_ONLY,
+    AUTHOR
+};
+
 const person = {
     name: 'Filip',
     age: 24,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN,
 };
+
+// person.role.push('admin');
+// person.role[1] = 10;
+// console.log(person.role);
 
 let favoriteActivites: string[];
 favoriteActivites = ['Sports'];
@@ -12,4 +24,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+    console.log("is admin");
 }
