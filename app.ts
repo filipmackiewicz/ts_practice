d2470c7995f3d95a3b0dd377a15f901d547c16e8
filipmackiewicz <mackiewicz.filip@gmail.com>
2022-07-10 12:03:17 +0200

    object type

--- app.ts
+++ app.ts
@@ -1,16 +1,6 @@
-function add(n1:number, n2:number, showResult:boolean, phrase:string) {
-    const result = n1 + n2;
-    if(showResult) {
-        console.log(phrase + result); 
-    } else {
-        return n1 + n2;
-    }
-}
+const person = {
+    name: 'Filip',
+    age: 24
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'result is: ';
-
-
-const result = add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
