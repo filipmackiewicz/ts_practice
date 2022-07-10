bce29264fac432e671582c6c6e03bba1e848b235
filipmackiewicz <mackiewicz.filip@gmail.com>
2022-07-10 13:04:50 +0200

    callbacks and fucntions

--- app.ts
+++ app.ts
@@ -7,6 +7,11 @@ function printResult (num: number):void {
     console.log('Result: ' + num);
 }
 
+function addAndHandle (n1: number, n2:number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
+
 printResult(addd(5,12));
 
 
@@ -16,3 +21,7 @@ combineValues = addd;
 // combineValues = printResult;
 
 console.log(combineValues(8,8));
+
+addAndHandle(10, 20, (result) => {
+    console.log(result);
+});
